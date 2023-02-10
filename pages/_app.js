2dddc328ec80dd177f01b13314641905a95046c1
Pierre ANDRESKI <pierre.andreski@gmail.com>
2023-02-09 21:03:05 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import { Open_Sans } from '@next/font/google'
+import '@/styles/globals.scss'
+
+const openSans = Open_Sans({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin']
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={openSans.className}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
