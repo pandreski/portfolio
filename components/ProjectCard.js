d0a3bd2429abcbb66a22babdb548b06f7c28a2dc
Pierre ANDRESKI <pierre.andreski@gmail.com>
2023-02-23 15:32:07 -0500

    Remove hover effect on mobile/tablet for ProjectCard

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 export default function ProjectCard({ title, image }) {
   return (
     <figure className='group relative w-full aspect-[600/382] rounded-lg overflow-hidden cursor-pointer'>
-      <Image src={image} alt={title} width='600' height='382' className='w-full h-full object-cover transition-transform group-hover:scale-105' />
-      <figcaption className='absolute p-4 inset-0 bg-deep-green text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+      <Image src={image} alt={title} width='600' height='382' className='w-full h-full object-cover transition-transform group-hover:lg:scale-105' />
+      <figcaption className='absolute p-4 inset-0 bg-deep-green text-white opacity-0 group-hover:lg:opacity-100 transition-opacity duration-300'>
         <span className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-serif text-lg md:text-2xl text-center'>
           {title}
         </span>
